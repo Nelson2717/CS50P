f107a8042e418983031bb8fffdc221e97cd71a58
Nelson Cruz <ncruz@aco.local>
2025-05-19 09:01:09 -0400

    .

--- hello2.py
+++ hello2.py
@@ -1,7 +1,10 @@
 # Create our own function
-def hello(to):
+def hello(to="world"):
     print("hello,", to )
 
 # Output using our own function
 name = input("What's your name? ")
 hello(name)
+
+# Output without passing the expected arguemnets
+hello()
